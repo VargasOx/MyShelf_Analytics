027a3b17b22df07aab1a1c099c90de801d39094e
VargasOx <marcelovargas13@gmail.com>
2024-07-14 21:28:05 -0300

    criação da estrutura de inserts nas tabelas autores e livros

--- criacao_estrutura.sql
+++ criacao_estrutura.sql
@@ -13,15 +13,15 @@
 create table autores
 
 (
-    id_autor int not null IDENTITY (1,1) PRIMARY KEY,
-    nome_autor varchar(250) not null,
-    pais_autor varchar(250) not null,
-    estado_autor varchar (250),
-    sexo_autor varchar (10) not null,
+    id_autor        int not null IDENTITY (1,1) PRIMARY KEY,
+    nome_autor      varchar(250) not null,
+    pais_autor      varchar(250) not null,
+    estado_autor    varchar(250),
+    sexo_autor      varchar(10) not null,
     data_nascimento date,
-    data_morte date,
-    principal_obra varchar(250),
-    data_cadastro date not null
+    data_morte      date,
+    principal_obra  varchar(250),
+    data_cadastro   date not null
 )
 
 /* cria a tabela de livros */
@@ -29,24 +29,27 @@ create table autores
 create table livros
 
 (
-    id_livro int not null IDENTITY (1,1) PRIMARY KEY,
-    titulo varchar (250) not null,
-    id_autor int not null,
-    editora VARCHAR(250) not null,
-    categoria varchar(250) not null,
-    tipo_edicao varchar(250) not null,
-    paginas int not null,
-    idioma varchar (20) not null,
-    isbn VARCHAR (50) not null,
-    status_leitura VARCHAR (10) not null,
-    data_conslusao date,
-    ano_lancamento int,
-    data_publicacao date not null,
-    valor_compra FLOAT,
-    data_cadastro date
+    id_livro        int not null IDENTITY (1,1) PRIMARY KEY,
+    titulo          varchar(250) not null,
+    id_autor        int not null,
+    editora         varchar(250) not null,
+    categoria       varchar(250) not null,
+    tipo_edicao     varchar(250) not null,
+    paginas         int not null,
+    idioma          varchar(20) not null,
+    isbn            varchar(50) not null,
+    status_leitura  varchar(15) not null,
+    data_conslusao  date,
+    ano_lancamento  int,
+    ano_publicacao  int not null,
+    valor_compra    float,
+    data_cadastro   date
 )
 
 /* cria a chave estrangeira id_autor na tabela livros */
 
 alter table livros ADD CONSTRAINT fk_autor FOREIGN KEY (id_autor) REFERENCES autores (id_autor)
+
+
+
 
