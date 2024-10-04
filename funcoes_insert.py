968cb661f509baa31fc8682934fa63bed203cd82
Marcelo <marcelovargas13@gmail.com>
2024-10-03 22:32:47 -0300

    Criação da função de inser_foto_autor no arquivo funcoes_insert

--- funcoes_insert.py
+++ funcoes_insert.py
@@ -11,7 +11,7 @@ def insert_autor(nome_autor,pais_autor,estado_autor,cidade_autor,sexo_autor,data
     ## cria a conexão com o banco de dados SQL Server.
 
     conexao = sql.connect(  'DRIVER={ODBC Driver 17 for SQL Server};' 
-                            'SERVER=LAPTOP-EI1CC4VU\SQLEXPRESS;' 
+                            'SERVER=LAPTOP-QTVOHOCT\SQLEXPRESS;' 
                             'DATABASE=db_my_shelf;'
                             'UID=;'
                             'PWD=;'
@@ -55,7 +55,7 @@ def insert_livro(titulo,id_autor,editora,categoria,edicao,reimpressao,tipo_edica
     ## cria a conexão com o banco de dados SQL Server.
 
     conexao = sql.connect(  'DRIVER={ODBC Driver 17 for SQL Server};' 
-                            'SERVER=LAPTOP-EI1CC4VU\SQLEXPRESS;' 
+                            'SERVER=LAPTOP-QTVOHOCT\SQLEXPRESS;' 
                             'DATABASE=db_my_shelf;'
                             'UID=;'
                             'PWD=;'
@@ -85,7 +85,7 @@ def insert_capa_livro():
 
 ## cria a conexão com o banco de dados SQL Server.
     conexao = sql.connect('DRIVER={ODBC Driver 17 for SQL Server};' 
-                      'SERVER=LAPTOP-EI1CC4VU\SQLEXPRESS;' 
+                      'SERVER=LAPTOP-QTVOHOCT\SQLEXPRESS;' 
                       'DATABASE=db_my_shelf;'
                       'UID=;'
                       'PWD=;'
@@ -101,8 +101,8 @@ def insert_capa_livro():
 
     ultimo_livro_cadastrado = cursor.execute('SELECT TOP 1 id_livro, isbn FROM livros order by id_livro desc').fetchall()
 
-    id_ultimo_livro= ultimo_livro_cadastrado[0][0]
-    isbn_ultimo_livro = ultimo_livro_cadastrado[0][1]
+    id_ultimo_livro     = ultimo_livro_cadastrado[0][0]
+    isbn_ultimo_livro   = ultimo_livro_cadastrado[0][1]
 
 
 ## chama a api das capas dos livros e concatena com a variavel isbn_ultimo_livro
@@ -124,5 +124,63 @@ def insert_capa_livro():
 
 
     conexao.execute(insert_capa,values)
+    conexao.commit()
+    conexao.close()
+
+
+
+
+
+
+
+
+
+
+## cria a função para inserir a capa do livro na tabela capas
+
+def insert_foto_autor():    
+
+## cria a conexão com o banco de dados SQL Server.
+    conexao = sql.connect('DRIVER={ODBC Driver 17 for SQL Server};' 
+                      'SERVER=LAPTOP-QTVOHOCT\SQLEXPRESS;' 
+                      'DATABASE=db_my_shelf;'
+                      'UID=;'
+                      'PWD=;'
+                      'TRUSTED_CONNECTION=yes'
+                    )
+    
+
+## chama a conexão com o banco e utiliza o método cursor (obrigatório)
+## faz uma query pra saber qual foi o último id_autor e nome_autor cadastrado ( neste caso busca o maior id autor)
+## armazena o resultado do id_autor e nome_autor nas variaveis id_ultimo_autor e nome_ultimo_autor
+
+    cursor = conexao.cursor()
+
+    ultimo_autor_cadastrado = cursor.execute('SELECT TOP 1 id_autor, nome_autor FROM autores order by id_autor desc').fetchall()
+
+    id_ultimo_autor     = ultimo_autor_cadastrado[0][0]
+    nome_ultimo_autor   = ultimo_autor_cadastrado[0][1]
+
+
+
+## chama a api das capas do wikipedia e concatena com a variavel isbn_ultimo_livro
+## armazena a resposta na variavel response já selecionando o campo url do dicionário do json que a API retorna
+
+    url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles={nome_ultimo_autor}&piprop=original"
+
+    response = ''.join(requests.get(url).json()['url'])
+    
+
+## faz o insert das variáves id_ultimo_autor, nome_ultimo_autor e response na tabela fotos_autores
+## se utiliza de parâmetros na parte do values do código, e passa as variáveis para a variável values 
+## o comando execute utiliza as duas varias veis criadas, insert_foto_autor e values, como parametro para executar o insert
+## executa o comando conexao.commit pois é obrigatório para comitar o insert na tabela
+## finaliza a conexão com o banco de dados
+
+    insert_foto_autor = 'INSERT foto_autores (id_autor,nome_autor,url_imagem) VALUES (?, ?, ?)'
+    values = (id_ultimo_autor,nome_ultimo_autor,response)
+
+
+    conexao.execute(insert_foto_autor,values)
     conexao.commit()
     conexao.close()
